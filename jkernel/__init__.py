eb057a26ccb995fda3a705372b32b68e4e8023fe
YuvaAthur <yuvaraj.a.r@gmail.com>
2016-04-11 18:40:25 -0700

    working set of jkernel files

---
+++ jkernel/__init__.py
@@ -0,0 +1,20 @@
+"""
+    jkernel
+    ~~~~~~~
+
+    Author:      Martin Saurer, 19.02.2016
+    Description: Jupyter / J - integration.
+
+    License:     GPL Version 3 (see gpl3.txt)
+    Extended by: Yuvaraj 11.04.2016
+"""
+# Imports
+import os
+import sys
+import time
+import base64
+
+# Append this files path to sys.path
+sys.path.append(os.path.dirname(__file__))
+
+
